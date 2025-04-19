81bede8e8cd99152bc7c0f6d6546e602b7ddc324
reiirene <hsinli@usc.edu>
2025-04-18 19:28:54 -0700

    Update nerf_transformer.py

--- pipeline/transformers/nerf_transformer.py
+++ pipeline/transformers/nerf_transformer.py
@@ -5,7 +5,7 @@ from ..models import ColmapOutput, NerfOutput
 from ..transformer import Transformer
 
 class NerfTransformer(Transformer[ColmapOutput, NerfOutput]):
-    def __init__(self, input_data: ColmapOutput, n_steps = 30000):
+    def __init__(self, input_data: ColmapOutput, n_steps: int = 30000):
         self.input_data = input_data
         self.n_steps = n_steps
 
@@ -18,6 +18,18 @@ class NerfTransformer(Transformer[ColmapOutput, NerfOutput]):
         self.snapshot_path = os.path.join(self.output_dir, f"{self.scene_name}_snapshot.msgpack")
         self.mesh_path = os.path.join(self.output_dir, f"{self.scene_name}_mesh.ply")
 
+    def transform(self) -> NerfOutput:
+        
+        self.run_ngp_training()
+        
+        return NerfOutput(
+            inner=input.inner + " nerf_transformed",
+            transforms_path=input.transforms_path,
+            colmap_path=input.colmap_path,
+            snapshot_path=self.snapshot_path,
+            mesh_path=self.mesh_path,
+        )
+    
     def run_ngp_training(self):
         run_path = os.path.join(self.ngp_dir, "scripts", "run.py")
         if not os.path.exists(run_path):
@@ -32,16 +44,3 @@ class NerfTransformer(Transformer[ColmapOutput, NerfOutput]):
             "--save_mesh", self.mesh_path,
             "--train"
         ], cwd=self.ngp_dir, check=True)
-
-    def transform(self, input: ColmapOutput) -> NerfOutput:
-        
-        self.run_ngp_training()
-        
-        return NerfOutput(
-            inner=input.inner + " nerf_transformed",
-            transforms_path=input.transforms_path,
-            colmap_path=input.colmap_path,
-            snapshot_path=self.snapshot_path,
-            mesh_path=self.mesh_path if os.path.exists(self.mesh_path) else None
-        )
-        #return NerfOutput(inner=input.inner + " nerf_transformed")
